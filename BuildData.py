fc8e2b5aebd2ef55db084f7b48a75c7f1b2b93f6
weanl <1975346264@qq.com>
2018-12-03 16:38:20 +0800

    modify the BuildData.py

--- BuildData.py
+++ BuildData.py
@@ -22,9 +22,28 @@ def fetchData():
     data = np.load('dataset1_v3.npy')
     print('data.shape: \t', data.shape)
 
+    return data
+
+def testData():
+
+    dataR = fetchData().reshape(-1, )
+    print('dataR.shape: \t', dataR.shape)
+
+    LEN = 1000
+    START = 3333
+    LookBack = 20
+
+    Pr = []
+    for i in range(LookBack):
+        start = START + i
+        data_tmp = dataR[start:start+LEN]
+        mask_tmp = (data_tmp == 1)
+        Pr.append(data_tmp[mask_tmp].shape[0])
+    print('Pr: \n', Pr)
+
+
+
     return 0
-
-
 
 
 
@@ -33,7 +52,8 @@ if __name__ == '__main__':
 
     print('*** BuildData ***')
 
-    fetchData()
+    #fetchData()
+    testData()
     #np.random.zipf()
 
 # END OF FILE
